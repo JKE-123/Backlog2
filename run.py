878e443f18c1c4f6e578f25e6253d256de5fea8e
JKE-123 <Jakey0ung@outlook.com>
2025-05-20 18:59:29 -0400

    comment out db init route

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 from app import app, db
 
 # Safe to remove this after initialization
-@app.route('/init-db')
-def init_db():
-  db.create_all()
-  return "Database initialized."
+#@app.route('/init-db')
+#def init_db():
+#  db.create_all()
+#  return "Database initialized."
