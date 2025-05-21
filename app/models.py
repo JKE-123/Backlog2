e60e2297b650d82ba5224f614eb230083e69d2e5
JKE-123 <Jakey0ung@outlook.com>
2025-05-21 10:24:09 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -4,9 +4,9 @@ from app import db
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
-    platform = db.Column(db.String(50))
+    #platform = db.Column(db.String(50))
     status = db.Column(db.String(50))  # e.g., "Not Started", "Playing", "Completed"
-    completed_year = db.Column(db.Integer, nullable=True)  # Optional: For chart grouping
-    steam_id = db.Column(db.String(50), nullable=True)     # Optional: Steam integration
-    rawg_id = db.Column(db.String(50), nullable=True)
-    hltb_id = db.Column(db.String(50), nullable=True)
+    time_played = db.Column(db.Integer, nullable=True)  # Optional: For chart grouping
+    #steam_id = db.Column(db.String(50), nullable=True)     # Optional: Steam integration
+    #rawg_id = db.Column(db.String(50), nullable=True)
+    #hltb_id = db.Column(db.String(50), nullable=True)
